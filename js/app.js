ab8b6ca6d0cdf8631a4918c993639712c14e73ae
Abel Quintero <abel.j.quintero@gmail.com>
2018-10-30 15:24:40 -0700

    renders the pokemon table by flowing down from pokemonTable init

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
-/*eslint-disable no-console */
+/* eslint-disable no-console */
 
 import pokemonApi from './pokemon-api.js';
+import pokemonTable from './pokemon-table.js';
 
 //get the initial data
 const pokemonData = pokemonApi.getAll();
 
 console.log(pokemonData);
+
+//data flows down
+pokemonTable.init(pokemonData);
+console.log(pokemonTable);
