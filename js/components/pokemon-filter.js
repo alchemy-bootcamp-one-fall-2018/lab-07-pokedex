6337365dbe4c7808cba63a90db2055e8d0329086
Cari Palazzolo <cari.pizza@gmail.com>
2018-10-30 16:28:52 -0700

    referenced filter inputs from DOM and added to pokemon-filter.js for export

--- js/components/pokemon-filter.js
+++ js/components/pokemon-filter.js
@@ -1,2 +1,33 @@
 // reference static DOM so next line knows about it
-const nameInput
+const nameInput = document.getElementById('filter-name');
+const type1Input = document.getElementById('filter-type1');
+const type2Input = document.getElementById('filter-type2');
+const speedInput = document.getElementById('filter-speed');
+const attackInput = document.getElementById('filter-attack');
+
+const pokemonFilter = {
+    // next line is what it needs from parent
+    init(onFilter) {
+        nameInput.addEventListener('keyup', function() {
+            onFilter(nameInput.value, type1Input.value, type2Input.value, speedInput.value, attackInput.value);
+        });
+
+        type1Input.addEventListener('keyup', function() {
+            onFilter(nameInput.value, type1Input.value, type2Input.value, speedInput.value, attackInput.value);
+        });
+
+        type2Input.addEventListener('keyup', function() {
+            onFilter(nameInput.value, type1Input.value, type2Input.value, speedInput.value, attackInput.value);
+        });
+
+        speedInput.addEventListener('keyup', function() {
+            onFilter(nameInput.value, type1Input.value, type2Input.value, speedInput.value, attackInput.value);
+        });
+
+        attackInput.addEventListener('keyup', function() {
+            onFilter(nameInput.value, type1Input.value, type2Input.value, speedInput.value, attackInput.value);
+        });
+    }
+};
+
+export default pokemonFilter;
