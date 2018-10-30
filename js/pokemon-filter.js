63a3ceba736436e6b1f609217553f2141b90a67b
Paige Gorry <paigeegorry@gmail.com>
2018-10-30 16:34:48 -0700

    all filters work on searching attack, defense, and hp

--- js/pokemon-filter.js
+++ js/pokemon-filter.js
@@ -1,15 +1,25 @@
 const nameInput = document.getElementById('filter-name');
 const attackInput = document.getElementById('filter-attack');
+const defenseInput = document.getElementById('filter-defense');
+const hpInput = document.getElementById('filter-hp');
 
 const pokeFilter = {
 
     init(onFilter) {
         nameInput.addEventListener('keyup', function() {
-            onFilter(nameInput.value, attackInput.value);
+            onFilter(nameInput.value, attackInput.value, defenseInput.value, hpInput.value);
         });
 
         attackInput.addEventListener('keyup', function() {
-            onFilter(nameInput.value, attackInput.value);
+            onFilter(nameInput.value, attackInput.value, defenseInput.value, hpInput.value);
+        });
+        
+        defenseInput.addEventListener('keyup', function() {
+            onFilter(nameInput.value, attackInput.value, defenseInput.value, hpInput.value);
+        });
+
+        hpInput.addEventListener('keyup', function() {
+            onFilter(nameInput.value, attackInput.value, defenseInput.value, hpInput.value);
         });
     }
 };
