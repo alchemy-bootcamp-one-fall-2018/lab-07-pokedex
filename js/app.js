fb8f1cd8bbe6076a0945008c4d5300405efb2364
Shaba9 <shaba92@gmail.com>
2018-10-30 16:33:00 -0700

    filters to console

--- js/app.js
+++ js/app.js
@@ -2,20 +2,10 @@ import pokeApi from './pokemon-api.js';
 import generateTable from './poke-table.js';
 import filter from './pokemon-filter.js';
 
-
 const pokemon = pokeApi.getAll();
-const textBox = document.getElementById('textBox');
 
 generateTable.init(pokemon);
-// filter.init();
-
-textBox.addEventListener('keyup', function() {
-    console.log(pokemon[0]);
-    console.log('key', textBox.value);
-    if(pokemon[0].pokemon.includes(textBox.value)) {
-        console.log('yes');
-    }
-});
+filter.init();
 
 
 
--- js/pokemon-filter.js
+++ js/pokemon-filter.js
@@ -1,24 +1,23 @@
 import pokeApi from './pokemon-api.js';
 import generateTable from './poke-table.js';
 
+const pokemon = pokeApi.getAll();
 const textBox = document.getElementById('textBox');
-const select = document.getElementById('select');
+// const select = document.getElementById('select');
 
 const filter = {
     
     init() {
-        const pokemon = pokeApi.getAll();
-        const filtered = [];
 
         textBox.addEventListener('keyup', function() {
-            console.log('key');
+            let filtered = [];
             for(let j = 0; j < 50; j++) {
-                console.log(pokemon[j]);
-                // if(pokemon[j].select.value.includes(textBox.value)) {
-                //     filtered.push(pokemon[j]);
-                // } 
+                if(pokemon[j].pokemon.includes(textBox.value) && textBox.value) {
+                    filtered.push(pokemon[j]);
+                }
             }
-            generateTable.init(filtered);
+            // generateTable.init(filtered);
+            console.log('filtered', filtered);
         });
     }
 };
