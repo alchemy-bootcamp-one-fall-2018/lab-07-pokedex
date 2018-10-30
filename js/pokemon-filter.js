77b9c5395eb63869e92552f0a457240cabcd53fa
Lise Brackbill <menagerie@hevanet.com>
2018-10-30 16:25:42 -0700

    added filter

--- js/pokemon-filter.js
+++ js/pokemon-filter.js
@@ -1,1 +1,16 @@
-const 
+const type1Input = document.getElementById('filter-type1');
+const type2Input = document.getElementById('filter-type2');
+
+const pokemonFilter = {
+    init(onFilter) {
+        type1Input.addEventListener('keyup', function() {
+            onFilter(type1Input.value, type2Input.value);
+        });
+        type2Input.addEventListener('keyup', function() {
+            onFilter(type1Input.value, type2Input.value);
+        });
+
+    }   
+};
+
+export default pokemonFilter;
