00f0d6bce0187e2ddfb5fbd2def86a9aa1e6b6d2
Carmen V Ramos <carmenvramos@yahoo.com>
2018-10-30 16:25:35 -0700

    Added image to show on pokemon table

--- js/pokemon-table.js
+++ js/pokemon-table.js
@@ -4,6 +4,7 @@ const tableBody = document.getElementById('pokemon-body');
 
 function makeRow(pokemon) {
     return html`<tr>
+        <td> <img src="../assets/pokemon/${pokemon.id}.png" </td>
         <td>${pokemon.pokemon}</td>
         <td>${pokemon.type_1}</td>
         <td>${pokemon.type_2}</td>
