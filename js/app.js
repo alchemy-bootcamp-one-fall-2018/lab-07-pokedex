b037bd107e6c1cffb5d441bb68880ae07470ad22
Cari Palazzolo <cari.pizza@gmail.com>
2018-10-30 16:55:40 -0700

    updating stylesheets, correcting file links, troubleshooting app.js console error

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 import pokemonApi from './components/pokemon-api.js';
 import pokemonTable from './components/pokemon-table.js';
-
+import pokemonFilter from './components/pokemon-filter.js';
 
 // get inital data
 const pokemon = pokemonApi.getAll();
