02639a79fbb5806b7a655fdf3aadcfed2ff32c2b
Heather <unphurlng@gmail.com>
2018-10-31 08:17:45 -0700

    added more to pokemonFilter object - not yet done. not working.

--- js/pokemon-filter.js
+++ js/pokemon-filter.js
@@ -1,3 +1,5 @@
+/*  */
+
 const filterName = document.getElementById('filter-name');
 const filterType1 = document.getElementById('filter-type1');
 const filterType2 = document.getElementById('filter-type2');
@@ -6,13 +8,18 @@ const filterDefense = document.getElementById('filter-defense');
 const filterSpeed = document.getElementById('filter-speed');
 
 const pokemonFilter = {
-    init() {
-        function handleFilter() {
-
-        }
+    init(onFilter) {
+        pokemonFilter.addEventListener('keyup', function() {
+            onFilter(
+                filterName.value,
+                filterType1.value,
+                filterType2.value,
+                filterAttack.value,
+                filterDefense.value,
+                filterSpeed.value
+            );
+        });
     }
 };
-
-
 
 export default pokemonFilter;
