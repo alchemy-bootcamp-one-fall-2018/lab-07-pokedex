ac648b31b963e250a56b6fdba62b6c0b807ca799
bpkotowski <bighoopee@gmail.com>
2018-10-30 17:35:16 -0700

    fixed errors except spell checker

---
+++ js/components/pokemon-table.js
@@ -0,0 +1,34 @@
+import html from '/Users/bigho/alchemy/week2/lab-07-pokedex/js/html';
+
+const tablebody = document.getElementById('pokemon-body');
+
+function makeRow(pokemon) {
+    return html`<tr>
+        <!-- <td class="sort asc">First</td> -->
+        <td>${pokemon.pokemon}</td>
+        <td>${pokemon.Type1}</td>
+        <td>${pokemon.Type2}</td>
+        <td>${pokemon.Attack}</td>
+        <td>${pokemon.Shape}</td>
+    </tr>`;
+}
+
+const pokemonTable = {
+    init(pokemon) {
+        for(let i = 0; i < pokemon.length; i++) {
+            const tr = makeRow(pokemon[i]);
+            tablebody.appendChild(tr);
+        
+        }
+    },
+    update(pokemon) {
+        while(tablebody.lastElementChild) {
+            tablebody.lastElementChild.remove();
+        }
+
+        pokemonTable.init(pokemon);
+
+    }
+};
+
+export default pokemonTable;
