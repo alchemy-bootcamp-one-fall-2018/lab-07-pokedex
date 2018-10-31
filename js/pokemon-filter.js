9fcad4575de2c4d2bf5da3eb7575546561a4aaeb
Heather <unphurlng@gmail.com>
2018-10-31 07:36:42 -0700

    added styles

--- js/pokemon-filter.js
+++ js/pokemon-filter.js
@@ -1,8 +1,15 @@
-
+const filterName = document.getElementById('filter-name');
+const filterType1 = document.getElementById('filter-type1');
+const filterType2 = document.getElementById('filter-type2');
+const filterAttack = document.getElementById('filter-attack');
+const filterDefense = document.getElementById('filter-defense');
+const filterSpeed = document.getElementById('filter-speed');
 
 const pokemonFilter = {
     init() {
+        function handleFilter() {
 
+        }
     }
 };
 
