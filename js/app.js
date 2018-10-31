de3741a45c5b6721ee00b9a462f3bc9d73f41008
Cari Palazzolo <cari.pizza@gmail.com>
2018-10-31 13:59:47 -0700

    added No matching results pre and hidden behavior when filter returns no results

--- js/app.js
+++ js/app.js
@@ -9,19 +9,23 @@ const pokemon = pokemonApi.getAll();
 // send data downward
 pokemonTable.init(pokemon);
 
+var noResultsResponse = document.getElementById(['no-results']);
+var headers = document.getElementById('column-headings');
 
 // now send the events up
 // pokemonFilter.init(function(nameFilter, type1Filter, type2Filter, speedFilter, attackFilter, shapeFilter) {
-pokemonFilter.init(function(nameFilter, type1Filter, type2Filter, shapeFilter) {
+pokemonFilter.init(function(nameFilter, type1Filter, type2Filter, shapeFilter, speedFilter, attackFilter) {
 
     let filtered;
 
-    if(nameFilter || type1Filter || type2Filter || shapeFilter) {
+    if(nameFilter || type1Filter || type2Filter || shapeFilter || speedFilter || attackFilter) {
 
         nameFilter = nameFilter.toLowerCase();
         type1Filter = type1Filter.toLowerCase();
         type2Filter = type2Filter.toLowerCase();
         shapeFilter = shapeFilter.value;
+        speedFilter = speedFilter.value;
+        attackFilter = attackFilter.value;
 
         filtered = pokemon.filter(function(pokemon) {
 
@@ -33,16 +37,24 @@ pokemonFilter.init(function(nameFilter, type1Filter, type2Filter, shapeFilter) {
                 || pokemon.type_2.toLowerCase().includes(type2Filter);
             const hasShape = !shapeFilter
                 || pokemon.shape.includes(shapeFilter);
-            // const hasSpeed = !speedFilter
-            //     || pokemon.speed. --?-- .includes(speedFilter);
-            // const hasAttack = !attackFilter
-            //     || pokemon.attack. --?-- .includes(attackFilter);
-            return hasName && hasType1 && hasType2 && hasShape;
+            const hasSpeed = !speedFilter
+                || pokemon.speed >= speedFilter;
+            const hasAttack = !attackFilter
+                || pokemon.attack >= attackFilter;
+
+            return hasName && hasType1 && hasType2 && hasShape && hasSpeed && hasAttack;
         });
+    }
+    if(filtered.length === 0) {
+        // console.log('test');
+        noResultsResponse.innerHTML = 'No matching results';
+        headers.classList.add('hidden');
     }
     else {
         // if no then use the full list
         filtered = pokemon;
+        noResultsResponse.classList.add('hidden');
+        headers.classList.remove('hidden');
     }
 
         // hey table, update!
--- js/pokemon-filter.js
+++ js/pokemon-filter.js
@@ -3,34 +3,35 @@ const nameInput = document.getElementById('filter-name');
 const type1Input = document.getElementById('filter-type1');
 const type2Input = document.getElementById('filter-type2');
 const shapeInput = document.getElementById('filter-shape');
-// const speedInput = document.getElementById('filter-speed');
-// const attackInput = document.getElementById('filter-attack');
+const speedInput = document.getElementById('filter-speed');
+const attackInput = document.getElementById('filter-attack');
 
 const pokemonFilter = {
     // next line is what it needs from parent
     init(onFilter) {
         nameInput.addEventListener('keyup', function() {
-            onFilter(nameInput.value, type1Input.value, type2Input.value, shapeInput);
+            onFilter(nameInput.value, type1Input.value, type2Input.value, shapeInput, speedInput, attackInput);
         });
 
         type1Input.addEventListener('keyup', function() {
-            onFilter(nameInput.value, type1Input.value, type2Input.value, shapeInput);
+            onFilter(nameInput.value, type1Input.value, type2Input.value, shapeInput, speedInput, attackInput);
         });
 
         type2Input.addEventListener('keyup', function() {
-            onFilter(nameInput.value, type1Input.value, type2Input.value, shapeInput);
+            onFilter(nameInput.value, type1Input.value, type2Input.value, shapeInput, speedInput, attackInput);
         });
 
         shapeInput.addEventListener('keyup', function() {
-            onFilter(nameInput.value, type1Input.value, type2Input.value, shapeInput);
+            onFilter(nameInput.value, type1Input.value, type2Input.value, shapeInput, speedInput, attackInput);
         });
-        // speedInput.addEventListener('keyup', function() {
-        //     onFilter(nameInput.value, type1Input.value, type2Input.value, speedInput.value, attackInput.value, shapeInput);
-        // });
 
-        // attackInput.addEventListener('keyup', function() {
-        //     onFilter(nameInput.value, type1Input.value, type2Input.value, speedInput.value, attackInput.value, shapeInput);
-        // });
+        speedInput.addEventListener('keyup', function() {
+            onFilter(nameInput.value, type1Input.value, type2Input.value, shapeInput, speedInput, attackInput);
+        });
+
+        attackInput.addEventListener('keyup', function() {
+            onFilter(nameInput.value, type1Input.value, type2Input.value, shapeInput, speedInput, attackInput);
+        });
 
 
     }
