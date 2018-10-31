6cb24e8986d7362d7bb21a58ec82b7cf8538966e
Lance Owen Merrill <Lanceom@hawaii.edu>
2018-10-30 18:28:31 -0700

    deleted comments

--- js/filterPokemon.js
+++ js/filterPokemon.js
@@ -1,20 +1,12 @@
-
-// grabs input from textbox
 const nameInput = document.getElementById('filter-name');
 const typeInput = document.getElementById('filter-type');
 const type2Input = document.getElementById('filter-type2');
 const attackInput = document.getElementById('filter-attack');
 const defenseInput = document.getElementById('filter-defense');
-
-
-//creates object with function as a property, becoming method
-
-// 
+ 
 const pokemonFilter = {
 
     init(onFilter) {
-        //On keyPress event, call the onFilter function for current value of textbox
-        //event listener subscribes to events
 
         function handleFilter() {
             onFilter(nameInput.value, typeInput.value, type2Input.value, attackInput.value, defenseInput.value);
--- js/pokemon-table.js
+++ js/pokemon-table.js
@@ -1,11 +1,6 @@
-// imports HTML function from html file
 import html from './html.js';
 
-//defines where to place the template in the HTML DOC
-
 const tableBody = document.getElementById('pokemon-body');
-
-// Function creates and defines the template to place data in
 
 function makePokemon(pokemon) {
 
@@ -22,8 +17,6 @@ function makePokemon(pokemon) {
         </tr>`;
 }
 
-// When this function is called, iterate through all the pokemon and fill in the template with data and append to the HTML document
-
 const pokemonTable = {
 
     init(pokemon) {
@@ -33,12 +26,10 @@ const pokemonTable = {
             tableBody.appendChild(tr);
         }
     },
-    //removes all rows upon an updated search
     update(pokemon) {
         while(tableBody.lastElementChild) {
             tableBody.lastElementChild.remove();
         }
-        // creates new rows based on the search
         pokemonTable.init(pokemon);
 
     }
