f224aebc2ed23557f96791a74fb0357910ee01b5
Ivan <i.poblete915@gmail.com>
2018-10-30 21:05:30 -0700

    fixed filter file

--- js/pokemon-filter.js
+++ js/pokemon-filter.js
@@ -1,4 +1,4 @@
-const nameInput = document.getElementById('filter-name');
+const pokemonInput = document.getElementById('filter-name');
 const type1Input = document.getElementById('filter-type-1');
 const type2Input = document.getElementById('filter-type-2');
 const ability1Input = document.getElementById('filter-ability-1');
@@ -7,22 +7,22 @@ const shapeInput = document.getElementById('filter-shape');
 
 const pokemonFilter = {
     init(onFilter) {
-        nameInput.addEventListener('keyup', function() {
-            onFilter(nameInput.value, type1Input.value, type2Input.value, ability1Input.value, shapeInput.value);
+        pokemonInput.addEventListener('keyup', function() {
+            onFilter(pokemonInput.value, type1Input.value, type2Input.value, ability1Input.value, shapeInput.value);
         });
         type1Input.addEventListener('keyup', function() {
-            onFilter(nameInput.value, nameInput.value, type1Input.value, type2Input.value, ability1Input.value, shapeInput.value);
+            onFilter(pokemonInput.value, type1Input.value, type2Input.value, ability1Input.value, shapeInput.value);
         });
         type2Input.addEventListener('keyup', function() {
-            onFilter(nameInput.value, nameInput.value, type1Input.value, type2Input.value, ability1Input.value, shapeInput.value);
+            onFilter(pokemonInput.value, type1Input.value, type2Input.value, ability1Input.value, shapeInput.value);
         });
         ability1Input.addEventListener('keyup', function() {
-            onFilter(nameInput.value, nameInput.value, type1Input.value, type2Input.value, ability1Input.value, shapeInput.value);
+            onFilter(pokemonInput.value, type1Input.value, type2Input.value, ability1Input.value, shapeInput.value);
         });
         shapeInput.addEventListener('keyup', function() {
-            onFilter(nameInput.value, nameInput.value, type1Input.value, type2Input.value, ability1Input.value, shapeInput.value);
+            onFilter(pokemonInput.value, type1Input.value, type2Input.value, ability1Input.value, shapeInput.value);
         });
-        console.log(pokemonFilter);
+        //console.log(pokemonFilter);
     }
 };
 
