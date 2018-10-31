99bb658ceab0068154098e3191f8e7cf3c845145
Cari Palazzolo <cari.pizza@gmail.com>
2018-10-30 23:00:48 -0700

    fixing new filter console errors

--- js/app.js
+++ js/app.js
@@ -15,35 +15,41 @@ pokemonTable.init(pokemon);
 
 
 // now send the events up
-pokemonFilter.init(function(nameFilter, type1Filter, type2Filter, speedFilter, attackFilter, shapeFilter) {
+// pokemonFilter.init(function(nameFilter, type1Filter, type2Filter, speedFilter, attackFilter, shapeFilter) {
+pokemonFilter.init(function(nameFilter) {//, type1Filter, type2Filter) {
 
     let filtered;
 
-    // is there a filter?
-    if(nameFilter || type1Filter || type2Filter || speedFilter || attackFilter || shapeFilter) {
+    // if(nameFilter || type1Filter || type2Filter || speedFilter || attackFilter || shapeFilter) {
+    if(nameFilter) { // || type1Filter || type2Filter) {
         // yes, filter based on name
-        nameFilter = nameFilter.toLowercase();
+       // nameFilter = nameFilter.str.toLowercase();
+        // nameFilter = nameFilter;
+        // type1Filter = type1Filter.toLowercase();
+        // type2Filter = type2Filter.toLowercase();
 
         filtered = pokemon.filter(function(pokemon) {
-            const hasName = !nameFilter
-                || pokemon.toLowercase().includes(nameFilter);
-            const hasType1 = !type1Filter
-                || pokemon.type_1.toLowercase().includes(type1Filter);
-            const hasType2 = !type2Filter
-                || pokemon.type_2.toLowercase().includes(type2Filter);
-            const hasSpeed = !speedFilter
-                || pokemon.speed.toLowercase().includes(speedFilter);
-            const hasAttack = !attackFilter
-                || pokemon.attack.toLowercase().includes(attackFilter);
-            const hasShape = !shapeFilter
-            || pokemon.shape.toLowercase().includes(shapeFilter);
-            return hasName && hasType1 && hasType2 && hasSpeed && hasAttack && hasShape;
+
+
+            const hasName = !nameFilter || pokemon.name.includes(nameFilter);
+           // || pokemon.str.toLowercase().includes(nameFilter);  // NUMBERS DON'T NEED TO LOWERCASE FUNCTION
+            // const hasType1 = !type1Filter
+            //     || pokemon.type_1.toLowercase().includes(type1Filter);
+            // const hasType2 = !type2Filter
+            //     || pokemon.type_2.toLowercase().includes(type2Filter);
+            // const hasSpeed = !speedFilter
+            //     || pokemon.speed.toLowercase().includes(speedFilter);
+            // const hasAttack = !attackFilter
+            //     || pokemon.attack.toLowercase().includes(attackFilter);
+            // const hasShape = !shapeFilter
+            // || pokemon.shape.toLowercase().includes(shapeFilter);
+            return hasName; // && hasType1 && hasType2 && hasSpeed && hasAttack && hasShape;
         });
     }
-    else {
-        // if no then use the full list
-        filtered = pokemon;
-    }
+    // else {
+    //     // if no then use the full list
+    //     filtered = pokemon;
+    // }
 
         // hey table, update!
     pokemonTable.update(filtered);
