0e3b3562d1492230f4abf05490e175eb262c9090
Cheri A Portman <cheriportman@hotmail.com>
2018-10-30 21:31:02 -0700

    had to quit for the night

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
 import pokemonApi from './pokemon-api.js';
 import pokemonsTable from './pokemon-table.js';
+import pokemonsFilter from './pokemon-filter.js';
 
 const pokemons = pokemonApi.getAll();
 
-pokemonsTable.init(pokemons);
+pokemonsTable.init(pokemons);    //data down
 
-pokemonsFilter.init(function(nameFilter){
-    let filtered;
-
-    if(nameFilter) {
-        nameFilter = nameFilter.toLowerCase();   //filters based on name (pokemon.pokemon)
-    }
-})
+//data down
+ pokemonsFilter.init(function(pokemonFilter, type_1Filter, 
+    type_2Filter, hpFilter, ability_1Filter, ability_2Filter, 
+    ability_hiddenFilter) {
+        
+        let filtered;
+        
+        if(pokemonFilter || type_1Filter || type_2Filter 
+            || hpFilter || ability_1Filter || ability_2Filter
+            ||ability_hiddenFilter) {
+               
+                filtered = pokemons.filter(function(pokemon) {
+                    const hasPokemon = !pokemonsFilter || pokemon.  
+                });
+            }   
+});
