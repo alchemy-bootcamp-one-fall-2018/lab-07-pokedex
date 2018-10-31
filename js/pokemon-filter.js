3b723ee14c15b357abc598b4ac53969109a18d2a
Cari Palazzolo <cari.pizza@gmail.com>
2018-10-31 11:52:56 -0700

    added type2 filter with LowerCase

--- js/pokemon-filter.js
+++ js/pokemon-filter.js
@@ -1,7 +1,7 @@
 // reference static DOM elements so next lines know about them
 const nameInput = document.getElementById('filter-name');
 const type1Input = document.getElementById('filter-type1');
-// const type2Input = document.getElementById('filter-type2');
+const type2Input = document.getElementById('filter-type2');
 // const speedInput = document.getElementById('filter-speed');
 // const attackInput = document.getElementById('filter-attack');
 // const shapeInput = document.getElementById('filter-shape');
@@ -10,16 +10,16 @@ const pokemonFilter = {
     // next line is what it needs from parent
     init(onFilter) {
         nameInput.addEventListener('keyup', function() {
-            onFilter(nameInput.value, type1Input.value); // type2Input.value, speedInput.value, attackInput.value, shapeInput);
+            onFilter(nameInput.value, type1Input.value, type2Input.value);
         });
 
         type1Input.addEventListener('keyup', function() {
-            onFilter(nameInput.value, type1Input.value); // type2Input.value); //, speedInput.value, attackInput.value, shapeInput);
+            onFilter(nameInput.value, type1Input.value, type2Input.value);
         });
 
-        // type2Input.addEventListener('keyup', function() {
-        //     onFilter(nameInput.value, type1Input.value, type2Input.value); //, speedInput.value, attackInput.value, shapeInput);
-        // });
+        type2Input.addEventListener('keyup', function() {
+            onFilter(nameInput.value, type1Input.value, type2Input.value);
+        });
 
         // speedInput.addEventListener('keyup', function() {
         //     onFilter(nameInput.value, type1Input.value, type2Input.value, speedInput.value, attackInput.value, shapeInput);
