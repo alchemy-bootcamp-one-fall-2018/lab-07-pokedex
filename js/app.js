cb437b3f6716c8b764ebdcc0beba9026dd9b2a0e
Cindy Neilson <cindy.neilson@gmail.com>
2018-11-02 12:39:27 -0700

    trying to correct for failed prior commit

--- js/app.js
+++ js/app.js
@@ -10,55 +10,20 @@ pokemonFilter.init(function(nameFilter, type_1Filter) {
     let filtered;
 
     if(nameFilter || type_1Filter) {
-        nameFilter = nameFilter.toLowerCase();
 
         filtered = pokemon.filter(function(pokemon) {
             const hasName = !nameFilter
             || pokemon.pokemon.toLowerCase().includes(nameFilter);
 
             const hasType_1 = !type_1Filter
-            || type_1.type_1.toLowerCase().includes(type_1Filter);
+            || pokemon.type_1.toLowerCase().includes(type_1Filter);
 
-            return hasName;
-        });
-        filtered = pokemon.filter(function(pokemon) {
-            const hasName = !nameFilter
-                || pokemon.pokemon.toLowerCase().includes(nameFilter);
-    
-            return hasName;
+            return hasName && hasType_1;
         });
     }
+
     else {
         filtered = pokemon;
     }
     pokeTable.update(filtered);
 });
-
-type_1Filter.init(function(type_1Filter) {
-    let filtered;
-
-    if(type_1Filter) {
-        type_1Filter = type_1Filter.toLowerCase();
-        filtered = type_1.filter(function(type_1) {
-            const hasType_1 = !type_1Filter
-            || type_1.type_1.toLowerCase().includes(type_1Filter);
-
-            return hasType_1;
-        });
-        filtered = type_1.filtetype1Filterr(function(type_1) {
-            const hasType_1 = !type_1Filter
-                || type_1.type_1.toLowerCase().includes(type_1Filter);
-    
-            return hasType_1;
-        });
-    }
-    else {
-        filtered = Type_1;
-    }
-    pokeTable.update(filtered);
-});
-
-
-// if(type2Filter, attackFilter, defenseFilter) {
-    // nameFilter = nameFilter.toLowerCase();
-
--- js/pokemon-filter.js
+++ js/pokemon-filter.js
@@ -1,38 +1,17 @@
 const nameInput = document.getElementById('filter-name');
 const type_1Input = document.getElementById('filter-type_1');
-// const type2Input = document.getElementById('filter-type_2');
+// const type_2Input = document.getElementById('filter-type_2');
 // const attackInput = document.getElementById('filter-attack');
 // const defenseInput = document.getElementById('filter-defense');
 
 const pokemonFilter = {
     init(onFilter) {
 
-        nameInput.addEventListener('keyup', function() {
-            onFilter(nameInput.value);
-        });
+        function useFilter() {
+            onFilter(nameInput.value, type_1Input.value);
+        }
+        nameInput.addEventListener('keyup', useFilter); 
+        type_1Input.addEventListener('keyup', useFilter);
     }
 };
-
-const type_1Filter = {
-    init(onFilter) {
-
-        type_1Input.addEventListener('keyup', function() {
-            onFilter(type_1Input.value);
-        });
-    }
-};
-
-
-// type2Input.addEventListener('keyup', function() {
-//     onFilter(type2Input.value);
-// });
-
-// attackInput.addEventListener('keyup', function() {
-//     onFilter(attackInput.value);
-// });
-
-// defenseInput.addEventListener('keyup', function() {
-//     onFilter(defenseInput.value);
-// });
-
 export default pokemonFilter;
